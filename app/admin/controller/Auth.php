f0bf023fd3637f5cbc18154264959389a5d68a28
chenxin <2582526377@qq.com>
2020-08-04 16:05:36 +0800

    后端授权功能

--- app/admin/controller/Auth.php
+++ app/admin/controller/Auth.php
@@ -10,30 +10,31 @@ class Auth extends BaseController
 {
     public function login()
     {
-        if (request()->isPost()) {
-            $username = input("username");
-            $password = input("password");
-            $user = AdminModel::where([
-                'username' => $username,
-                'password' => md5($password)
-            ])->find();
-            if ($user) {
-                $str = md5(uniqid(md5(microtime(true)), true));
-                $token = sha1($str . $user->username);
-                Cache::set($token, ['user_id' => $user->id, 'username' => $user->username, 'name' => $user->name], 60 * 60 * 24);
-                $data = [
-                    'code' => 200,
-                    'message' => '',
-                    'result' => [
-                        'avatar' => "https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png",
-                        'id' => $user->id,
-                        'token' => $token,
-                        'username' => $user->username
-                    ],
-                    'timestamp' => time()
-                ];
-                return json($data);
-            }
+        $username = input("username");
+        $password = input("password");
+        $user = AdminModel::where([
+            'username' => $username,
+            'password' => md5($password)
+        ])->find();
+        if ($user) {
+            $token = UserToken::createToken($user, 'user');
+            $str = md5(uniqid(md5(microtime(true)), true));
+            $token = sha1($str . $user->username);
+            Cache::set($token, ['user_id' => $user->id, 'username' => $user->username, 'name' => $user->name], 60 * 60 * 24);
+            $data = [
+                'code' => 200,
+                'message' => '登陆成功',
+                'result' => [
+                    'avatar' => "https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png",
+                    'id' => $user->id,
+                    'token' => $token,
+                    'username' => $user->username
+                ],
+                'timestamp' => time()
+            ];
+            return json($data);
+        } else {
+            return json([ 'message' => '用户名或者密码错误'],401);
         }
     }
 
