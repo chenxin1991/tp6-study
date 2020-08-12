b32b45eb8ce4c0cdace361d21ac9eeedee71d5ec
chenxin <2582526377@qq.com>
2020-08-12 15:42:22 +0800

    物品管理可上传多张图片

--- app/models/Goods.php
+++ app/models/Goods.php
@@ -5,6 +5,10 @@ use think\Model;
 
 class Goods extends Model
 {
+    protected $json = ['image_url'];
+
+    protected $jsonAssoc = true;
+
     public function category()
     {
         return $this->belongsTo(Category::class,'cid')->bind(['category' => 'name']);
