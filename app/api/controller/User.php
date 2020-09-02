7c7af353803843ca515439f4665fd0beab5eb515
chenxin <2582526377@qq.com>
2020-09-02 15:40:07 +0800

    '添加微信端上传图片接口'

--- app/api/controller/User.php
+++ app/api/controller/User.php
@@ -2,6 +2,7 @@
 
 namespace app\api\controller;
 
+use app\BaseController;
 use app\api\model\User as UserModel;
 use think\Request;
 
@@ -24,7 +25,36 @@ class User extends BaseController
         $model = new UserModel;
         $user_id = $model->login($request->post());
         $token = $model->getToken();
-        return json($this->renderSuccess(compact('user_id', 'token')));
+        return json(['code' => 1, 'data' => ['token' => $token, 'user_id' => $user_id], 'msg' => 'success']);
+    }
+
+    public function detail()
+    {
+        // 当前用户信息
+        if (!$token = request()->param('token')) {
+            return json(['code' => -1, 'msg' => '缺少必要的参数：token']);
+        }
+        if (!$userInfo = UserModel::getUser($token)) {
+            return json(['code' => -1, 'msg' => '没有找到用户信息']);
+        }
+        return json(['code' => 1, 'data' => ['userInfo' => $userInfo], 'msg' => 'success']);
+    }
+
+    public function uploadImage()
+    {
+        // 当前用户信息
+        if (!$token = request()->param('token')) {
+            return json(['code' => -1, 'msg' => '缺少必要的参数：token']);
+        }
+        if (!$userInfo = UserModel::getUser($token)) {
+            return json(['code' => -1, 'msg' => '没有找到用户信息']);
+        }
+        $file = request()->file('file');
+        $savename = \think\facade\Filesystem::disk('public')->putFile('wechat', $file);
+        return json([
+            'code' => '1',
+            'image_url' => $this->app->config->get('app.app_host') . '/storage/' . str_replace('\\', '/', $savename)
+        ]);
     }
 
 }
