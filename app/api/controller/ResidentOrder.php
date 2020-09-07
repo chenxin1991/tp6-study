5877ef6fb2d27739cc8777f5b8a43ec02f0c0119
chenxin <2582526377@qq.com>
2020-09-07 15:33:44 +0800

    '修改bug'

--- app/api/controller/ResidentOrder.php
+++ app/api/controller/ResidentOrder.php
@@ -25,7 +25,7 @@ class ResidentOrder extends BaseController
         } else {
             $orderNumber = $date . '0001';
         }
-        $source = 3;
+        $source = 3;//小程序
         $customer = $userInfo['nickName'];
         $phone = input('mobile');
         $appointDate = input('appointDate');
@@ -34,6 +34,8 @@ class ResidentOrder extends BaseController
         $cars = json_decode(htmlspecialchars_decode($cars), true);
         foreach ($cars as $key => $value) {
             $cars[$key]['key'] = $key;
+            $cars[$key]['id'] = intval(substr($value['id'], 4));
+            $cars[$key]['total'] = (float)($value['num'] * $value['price']);
         }
         $routes = [];
         $addressFrom = input('addressFrom');
@@ -47,9 +49,9 @@ class ResidentOrder extends BaseController
                 'lng' => $addressFrom['address']['longitude']
             ],
             'room_number' => $addressFrom['room_number'],
-            'stairs_or_elevators' => $addressFrom['stairs_or_elevators'],
+            'stairs_or_elevators' => strval($addressFrom['stairs_or_elevators']),
             'floor_num' => $addressFrom['floor_num'],
-            'parking_distance' => $addressFrom['parking_distance']
+            'parking_distance' => strval($addressFrom['parking_distance'])
         ];
         $addressTo = input('addressTo');
         $addressTo = json_decode(htmlspecialchars_decode($addressTo), true);
@@ -62,22 +64,23 @@ class ResidentOrder extends BaseController
                 'lng' => $addressTo['address']['longitude']
             ],
             'room_number' => $addressTo['room_number'],
-            'stairs_or_elevators' => $addressTo['stairs_or_elevators'],
+            'stairs_or_elevators' => strval($addressTo['stairs_or_elevators']),
             'floor_num' => $addressTo['floor_num'],
-            'parking_distance' => $addressTo['parking_distance']
+            'parking_distance' => strval($addressTo['parking_distance'])
         ];
         $goods = [];
         $i = 0;
         $cart = input('cart');
         $cart = json_decode(htmlspecialchars_decode($cart), true);
         foreach ($cart as $key => $value) {
-            if(!strstr($value['id'],'car_')){
+            if (!strstr($value['id'], 'car_')) {
                 $cart[$key]['key'] = $i;
+                $cart[$key]['total'] = (float)($value['num'] * $value['price']);
                 $goods[] = $cart[$key];
                 $i++;
             }
         }
-        $distance = input('distance');
+        $distance = (float)(input('distance'));
         $carCost = input('carCost');
         $distanceCost = input('distanceCost');
         $goodsCost = input('largeCost');
@@ -89,28 +92,25 @@ class ResidentOrder extends BaseController
         $residentOrder = new ResidentOrderModel;
         $residentOrder->number = $orderNumber;
         $residentOrder->source = $source;
-        $residentOrder->appointDate = $appointDate;
-        $residentOrder->appointTime = $appointTime;
         $residentOrder->customer = $customer;
         $residentOrder->phone = $phone;
+        $residentOrder->appointDate = $appointDate;
+        $residentOrder->appointTime = $appointTime;
         $residentOrder->cars = $cars;
         $residentOrder->routes = $routes;
         $residentOrder->goods = $goods;
         $residentOrder->distance = $distance;
         $residentOrder->carCost = $carCost;
         $residentOrder->distanceCost = $distanceCost;
+        $residentOrder->goodsCost = $goodsCost;
         $residentOrder->floorCost = $floorCost;
         $residentOrder->parkingCost = $parkingCost;
-        $residentOrder->goodsCost = $goodsCost;
         $residentOrder->specialTimeCost = $specialTimeCost;
         $residentOrder->totalCost = $totalCost;
-        $residentOrder->operator = $operator;
-        if ($source == 2) {
-            $residentOrder->orderStatus = 0;
-        } else {
-            $residentOrder->orderStatus = 1;
-        }
-        $residentOrder->payStatus = 0;
+        $residentOrder->user_id = $user_id;
+        $residentOrder->orderStatus = 0;//待确认
+        $residentOrder->payStatus = 0;//待支付
         $residentOrder->save();
+        return json(['code' => 1, 'msg' => 'success']);
     }
 }
--- app/models/ResidentOrder.php
+++ app/models/ResidentOrder.php
@@ -22,15 +22,9 @@ class ResidentOrder extends Model
         return $this->belongsTo(Leader::class, 'leader')->bind(['leadername' => 'name']);
     }
 
-    public function getSourceAttr($value)
-    {
-        $status = [0 => '来电', 1 => '上门', 2 => '小程序'];
-        return $status[$value];
-    }
-
     public function getOrderStatusAttr($value)
     {
-        $status = [0 => '待受理', 1 => '待派单', 2 => '待开工', 3 => '待完成', 4 => '待评价', 5 => '已关闭', 6 => '已取消'];
+        $status = [-1 => '已取消', 0 => '待确认', 1 => '待派单', 2 => '待开工', 3 => '待完工', 4 => '待评价', 5 => '已完成'];
         return $status[$value];
     }
 
