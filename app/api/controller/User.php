d0d74cec9e9e55b37d7cba8c21ddbce5ee94a8ac
chenxin <2582526377@qq.com>
2020-09-15 13:41:25 +0800

    '测试'

--- app/api/controller/User.php
+++ app/api/controller/User.php
@@ -45,7 +45,7 @@ class User extends BaseController
                 return json(['code' => 1, 'data' => [], 'msg' => 'success']);
             }
         } else {
-            return json(['code' => 0, 'data' => [], 'msg' => $errCode]);
+            return json(['code' => 0, 'data' => [], 'msg' => $sessionKey]);
         }
     }
 
