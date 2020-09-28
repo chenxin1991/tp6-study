66f7f77736ac8bf80b18497c88adcc2b152d25c3
chenxin <2582526377@qq.com>
2020-09-28 15:06:15 +0800

    '完善代码'

--- app/api/controller/CarCategory.php
+++ app/api/controller/CarCategory.php
@@ -4,17 +4,20 @@ namespace app\api\controller;
 
 use app\BaseController;
 use app\models\Car as CarModel;
+use app\models\Setting as SettingModel;
 
 class CarCategory extends BaseController
 {
     public function index()
     {
         $carCategory = CarModel::select();
+        $setting = SettingModel::find(1);
         $result = [
             'status' => 200,
             'msg' => 'ok',
             'data' => [
-                'carCategory' => $carCategory
+                'carCategory' => $carCategory,
+                'setting' => $setting
             ]
         ];
         return json($result);
